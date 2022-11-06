486fd87de3eb9c09405870abcfd9d133d7b77bf8
Oliverio <oliverio.junior2@gmail.com>
2022-11-06 20:27:12 -0300

    subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 /* import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'; */
@@ -8,8 +10,16 @@ interface SubscribeButtonProps {
 
 export const SubscribeButton: React.FC<SubscribeButtonProps> = () => {
   const { status } = useSession();
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (status === 'unauthenticated') signIn('github');
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe?.redirectToCheckout(sessionId);
+    } catch (err) {
+      alert(err);
+    }
   }
 
   return (
